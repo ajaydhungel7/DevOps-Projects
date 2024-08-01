941f3e1238345cdb556a11f4cb8a76ab62d05a77
Ajay Dhungel <ajaydhungel@Ajay-PC.local>
2024-08-01 17:01:45 -0400

    more permission related changes on acr

--- iac/main.tf
+++ iac/main.tf
@@ -67,15 +67,16 @@ resource "azurerm_role_assignment" "aks_user_role" {
 }
 
 resource "azurerm_role_assignment" "acr_pull_role" {
-  scope                = azurerm_resource_group.rg.id
+  scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull" 
   principal_id         = azurerm_user_assigned_identity.vm_identity.principal_id
 }
 
 resource "azurerm_role_assignment" "acr_push_role" {
-  scope                = azurerm_resource_group.rg.id
+  scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPush" 
   principal_id         = azurerm_user_assigned_identity.vm_identity.principal_id
+  skip_service_principal_aad_check = true
 }
 
 resource "azurerm_container_registry" "acr" {
